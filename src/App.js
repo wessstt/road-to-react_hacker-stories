e541b99488f7efa0bdf00d290244f96ee1abdc82
Keri West <keriwest88@gmail.com>
2023-10-19 11:51:31 -0400

    props, state and callback handlers.

--- src/App.js
+++ src/App.js
@@ -1,104 +1,84 @@
 import * as React from "react";
 
-const list = [
-  {
-    title: "React",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+// Concise JSX component with arrow functions
+const App = () => {
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
 
-// original function declaration
-// function List() {
-//    return (
-//      <ul>
-//        {list.map(function (item) {
-//          return (
-//            <li key={item.objectID}>
-//              <span>
-//                <a href={item.url}>{item.title}</a>
-//              </span>
-//              <span>{item.author}</span>
-//              <span>{item.num_comments}</span>
-//              <span>{item.points}</span>
-//             </li>
-//           );
-//         })}
-//       </ul>
-//     );
-//   }
-  
+  const handleSearch = (event) => {
+    console.log(event.target.value);
+  };
+
+  return (
+    <div>
+      <h1>My Hacker Stories</h1>
+
+      <Search onSearch={handleSearch} />
+
+      <hr />
+
+      <List list={stories} />
+    </div>
+  );
+};
 
 // Concise JSX component with arrow functions
-const List = () => (
+const Search = (props) => {
+  const [searchTerm, setSearchTerm] = React.useState("");
+
+  const handleChange = (event) => {
+    //console.log(`Before setting searchTerm: ${searchTerm}`);
+    setSearchTerm(event.target.value);
+    //console.log(`After setting searchTerm: ${searchTerm}`);
+    props.onSearch(event);
+  };
+
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={handleChange} />
+
+      <p>
+        Searching for <strong>{searchTerm}</strong>
+      </p>
+    </div>
+  );
+};
+
+// Concise JSX component with arrow functions
+const List = (props) => (
   <ul>
-    {list.map((item) => (
-      <li key={item.objectID}>
-        <span>
-          <a href={item.url}>{item.title}</a>
-        </span>
-        <span>{item.author}</span>
-        <span>{item.num_comments}</span>
-        <span>{item.points}</span>
-      </li>
+    {props.list.map((item) => (
+      <Item key={item.objectID} item={item} />
     ))}
   </ul>
 );
 
-// original function declaration
-// function Search() {
-//   return (
-//    <div> 
-//     <label htmlFor="search">Search: </label>
-//     <input id="search" type="text" />
-//    </div>
-//    )
-//  };
-
-
-// Concise JSX component with arrow functions
-const Search = () => (
-  <div>
-    <label htmlFor="search">Search: </label>
-    <input id="search" type="text" />
-  </div>
-);
-
-// original function declaration
-// function App() {
-//  return (
-//    <div>
-//      <h1>My Hacker Stories</h1>
-
-//      <Search />
-
-//      <hr />
-
-//      <List />
-//    </div>
-//    );
-//  }
-
-
-// Concise JSX component with arrow functions
-const App = () => (
-  <div>
-    <h1>My Hacker Stories</h1>
-    <Search />
-    <hr />
-    <List />
-  </div>
+const Item = (props) => (
+  <li>
+    <span>
+      <a href={props.item.url}>{props.item.title}</a>
+    </span>
+    <span>{props.item.author}</span>
+    <span>{props.item.num_comments}</span>
+    <span>{props.item.points}</span>
+  </li>
 );
 
 export default App;
